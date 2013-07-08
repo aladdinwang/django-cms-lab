834a936832eca9e03798b17f1ab2158fbf2ca3ab
aladdin <aladdin@centos-aladdin.(none)>
2013-07-08 18:30:59 +0800

    add media, new html

--- pagedesign/views.py
+++ pagedesign/views.py
@@ -3,14 +3,48 @@ from django.http import HttpResponse
 from django.contrib.formtools.wizard.views import SessionWizardView
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+from django.template.loader import get_template, render_to_string
+from django.utils.safestring import mark_safe
+from django.template import Context
 from pagedesign.forms import MasterPageSelectForm, RowsInfoForm
 
 class MasterPageSelectWizard( SessionWizardView ):
     picture_row_snippet_choices = (
-        ( 'snippets/picture_2_column.html', _('Picture with 2 columns') ),
-        ( 'snippets/picture_3_column.html', _('Picture with 3 columns') ),
+        ( 2, _('Picture with 2 columns') ),
+        ( 3, _('Picture with 3 columns') ),
         )
+    base_template_name = "base/photography.html"
     def done( self, form_list, **kwargs ):
+        placeholder_template = " placeholder %(placeholder_name)s group=image tips=%(placeholder_tips)s " \
+            "width=216 height=168 "
+        cleaned_data = self.get_all_cleaned_data()
+        #print cleaned_data
+        row_count = int( cleaned_data.get( 'rows', 0 ) )
+        picture_rows = []
+        picture_id = 1
+
+        for row in range( 1, row_count + 1 ):
+            key = 'row' + str( row )
+            # may use get
+            picture_cols = []
+            col_count = int( cleaned_data[ key ] )
+            for col in range( 0, col_count  ):
+                picture = {}
+                substitution = {
+                    'placeholder_name':  "photo%s" % str( picture_id ),
+                    'placeholder_tips':  "Design for photo%s" % ( str( picture_id ) ),
+                }
+                picture_id += 1
+                picture[ 'placeholder' ] = placeholder_template % substitution
+                picture[ 'url' ] = '#'
+                picture[ 'name' ] = substitution[ 'placeholder_name' ]
+                picture_cols.append( picture )
+                
+            picture_rows.append( picture_cols )
+        
+        
+        template = render_to_string( self.base_template_name, Context( { 'picture_rows': picture_rows } ) )
+        print mark_safe( template )
         return HttpResponse( "OK"  )
     def get_form( self, step = None, data = None, files = None ):
         if '1' == step:
